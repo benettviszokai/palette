d5dc1b8d5ec69f4aa1e01bed508969a794792066
viszokaib <viszokaib@gmail.com>
2022-06-04 12:11:32 +0200

    06 - coloring our inputs

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ function randomColors() {
 
         // Check contrast
         checkTextContrast(randomColor, hexText);
+
+        // Initial colorize sliders
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll('.sliders input');
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+        colorizeSliders(color, hue, brightness, saturation);
     });
 };
 
@@ -31,6 +40,24 @@ function randomColors() {
 function checkTextContrast(color, text) {
     const luminance = chroma(color).luminance();
     luminance > 0.5 ? text.style.color = "black" : text.style.color = "white";
-}
+};
+
+function colorizeSliders(color, hue, brightness, saturation) {
+    // Scale saturation
+    const noSaturation = color.set('hsl.s', 0);
+    const fullSaturation = color.set('hsl.s', 1);
+    const scaleSaturation = chroma.scale([noSaturation, color, fullSaturation]);
+
+    // Scale Brightness
+    const midBrightness = color.set('hsl.l', 0.5);
+    const scaleBrightness = chroma.scale(['black', midBrightness, 'white']);
+
+    // Scale Hue - not necessary
+
+    // Update input colors
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSaturation(0)}, ${scaleSaturation(1)})`;
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBrightness(0)}, ${scaleBrightness(0.5)}, ${scaleBrightness(1)})`;
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204, 75, 75), rgb(204, 204, 75), rgb(75, 204, 75), rgb(75, 204, 204), rgb(75, 75, 204), rgb(204, 75, 204), rgb(204, 75, 75))`;
+};
 
 randomColors();
