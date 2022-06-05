64e0cbed52b156259eb2f55232c3af647b2dc84a
viszokaib <viszokaib@gmail.com>
2022-06-05 07:39:22 +0200

    12. copy to clipboard

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const colorDivs = document.querySelectorAll('.color');
 const generateBtn = document.querySelector('.generate');
 const sliders = document.querySelectorAll('input[type="range"]');
-const currentHexes = document.querySelector('.color h2');
+const currentHexes = document.querySelectorAll('.color h2');
+const popup = document.querySelector('.copy-container');
 let initialColors;
 
 // ------------------------------- Event listeners ------------------------------- //
@@ -17,6 +18,17 @@ colorDivs.forEach((div, index) => {
     });
 });
 
+currentHexes.forEach(hex => {
+    hex.addEventListener("click", () => {
+      copyToClipboard(hex);
+    });
+});
+
+popup.addEventListener('transitioned', () => {
+    const popupBox = popup.children[0];
+    popup.classList.remove('active');
+    popupBox.classList.remove('active');
+});
 // ------------------------------- Functions ------------------------------- //
 
 // Generating colors using chroma.js
@@ -137,6 +149,19 @@ function resetInputs() {
             slider.value = Math.floor(satValue * 100) / 100;
         };
     });
+};
+
+function copyToClipboard(hex) {
+    const el = document.createElement("textarea");
+    el.value = hex.innerText;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+    //Pop up animation
+    const popupBox = popup.children[0];
+    popup.classList.add("active");
+    popupBox.classList.add("active");
 };
 
 randomColors();
